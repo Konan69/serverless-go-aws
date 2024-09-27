2477b96b0f923bfe81a21b1bf036e554f5804189
Konan <kixeyems0@gmail.com>
2024-09-27 19:09:07 +0100

    updates

---
+++ lambda/api/api.go
@@ -0,0 +1,15 @@
+package api
+
+import (
+	"lambda-func/database"
+)
+
+type ApiHandler struct {
+	dbStore database.DynamoDbClient
+}
+
+func NewApiHandler(dbStore database.DynamoDbClient) ApiHandler {
+	return ApiHandler{
+		dbStore: dbStore,
+	}
+} 
--- lambda/database/database.go
+++ lambda/database/database.go
@@ -1,11 +1,64 @@
 package database
 
+import (
+	"lambda-func/types"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+const (
+	tableName = "userTable"
+)
 type DynamoDbClient struct {
-	databaseStore string
+	databaseStore *dynamodb.DynamoDB
 }
 
 func NewDynamoDbClient() DynamoDbClient {
+	dbSession := session.Must(session.NewSession())
+	db := dynamodb.New(dbSession)
 	return DynamoDbClient{
-		databaseStore: "dynamodb",
+		databaseStore: db,
 	}
+}
+
+func (u DynamoDbClient) DoesUserExist(username string) (bool, error) {
+	result, err := u.databaseStore.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(username),
+			},
+		},
+	})
+
+	if err != nil {
+		return true, err
+	}
+	if result.Item == nil {
+		return false, nil
+	}
+	return true, nil
+}
+
+
+func (u DynamoDbClient) CreateUser(user types.RegisterUser) error {
+
+	item := &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]*dynamodb.AttributeValue{
+			"username": {
+				S: aws.String(user.Username),
+			},
+			"email": {
+				S: aws.String(user.Password),
+			},
+		},
+	}
+	_, err := u.databaseStore.PutItem(item)
+	if err != nil {
+		return err
+	}
+	return nil  
 }
